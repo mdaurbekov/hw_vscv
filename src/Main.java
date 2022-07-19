e41937b35ab2167522901891d08ae6249736ab60
mdaurbekov <dreamkov@gmail.com>
2022-07-19 18:57:28 +0300

    12

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,5 @@ public class Main {
     int[] array = new int[]{1, 2, 3, 4};
 
     SalesManager salesManager = new SalesManager(array);
-    //Hello
+    //hel
 }
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -16,5 +16,5 @@ public class SalesManager {
         }
         return max;
     }
-    //
+    // 1
 }
