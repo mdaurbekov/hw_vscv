7d1310d8ee21bc2c788fc5c3b58ff52f3d69b230
mdaurbekov <dreamkov@gmail.com>
2022-07-20 17:14:59 +0300

    121

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 package src;
 
 public class Main {
-    int[] array = new int[]{1, 2, 3, 4};
+    long[] array = new long[]{1, 2, 3, 4};
 
-    int max = new SalesManager(array).max();
+    long max = new SalesManager(array).max();
 
 
 
