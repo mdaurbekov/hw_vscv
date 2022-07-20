dec1188ae1edeae53340552e432d3457c8fdcfdd
mdaurbekov <dreamkov@gmail.com>
2022-07-20 18:23:40 +0300

    121

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 4};
 
-        int mean = new SalesManager(array).mean();
+        long mean = new SalesManager(array).mean();
 
         System.out.println(mean);
 
--- src/SalesManager.java
+++ src/SalesManager.java
@@ -16,9 +16,9 @@ public class SalesManager {
         }
         return max;
     }
-    public int min() {
-        int min = sales[0];
-        for (int sale : sales) {
+    public long min() {
+        long min = sales[0];
+        for (long sale : sales) {
             if (sale < min) {
                 min = sale;
             }
@@ -27,10 +27,10 @@ public class SalesManager {
     }
 
 
-    public int mean(){
-        int sum = 0;
+    public long mean(){
+        long sum = 0;
         int count = 0;
-        for (int sale: sales){
+        for (long sale: sales){
             if (sale != min() && sale != max()){
                 sum += sale;
                 count++;
